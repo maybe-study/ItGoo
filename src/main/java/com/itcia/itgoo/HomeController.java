f01e634548323e4c32aff168d8546c0725d27eaf
maybe-wtudy <han660913@naver.com>
2020-02-28 13:30:39 +0900

    홈 컨트롤러 변경

--- src/main/java/com/itcia/itgoo/HomeController.java
+++ src/main/java/com/itcia/itgoo/HomeController.java
@@ -10,18 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	//민호
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
