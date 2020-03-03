0860fa0c02eed9d7b27dee833605e0925a12cd60
PYOUNG-MINHO <59430673+PYOUNG-MINHO@users.noreply.github.com>
2020-03-03 14:41:36 +0900

    로그인
    로그인 부트스트랩

--- src/main/java/com/itcia/itgoo/AdminController.java
+++ src/main/java/com/itcia/itgoo/AdminController.java
@@ -15,18 +15,6 @@ public class AdminController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	//민호
 	//병규
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	
 	
 }
