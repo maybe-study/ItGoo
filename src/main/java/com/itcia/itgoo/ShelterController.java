4006fc700b67797c8774b2b71e7a6258ba231562
ParkByungKyu-dot <tizm5gh@naver.com>
2020-03-05 09:19:25 +0900

    위치 바꿈

--- src/main/java/com/itcia/itgoo/ShelterController.java
+++ src/main/java/com/itcia/itgoo/ShelterController.java
@@ -6,25 +6,46 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.itcia.itgoo.dto.Member;
 
 @Controller
 public class ShelterController {
+
+	@Autowired
+	private MemberManagement mm;
+	ModelAndView mav = new ModelAndView();
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
 	@RequestMapping(value = "/joinfrm", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "main";
 	}
+
+	// 회원가입
+	@RequestMapping(value = "/sheltermemberjoin", method = RequestMethod.POST)
+	public ModelAndView memberjoin(Member mb) {
+
+		mav = mm.sheltermemberjoin(mb);
+
+		return mav;
+	}
+
 }
---
+++ src/main/java/com/itcia/itgoo/dao/IShelterDao.java
@@ -0,0 +1,11 @@
+package com.itcia.itgoo.dao;
+
+import com.itcia.itgoo.dto.Member;
+
+public interface IShelterDao {
+
+	void sheltermemberJoin(Member mb);
+
+	
+
+}
