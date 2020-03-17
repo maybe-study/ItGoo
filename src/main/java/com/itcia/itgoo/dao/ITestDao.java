dcd65be0908b3cbafb8ec05571ea18f19d95ebbf
zzidoc94 <zzidoc94@gmail.com>
2020-03-17 16:21:33 +0900

    test
    완료

--- src/main/java/com/itcia/itgoo/dao/ITestDao.java
+++ src/main/java/com/itcia/itgoo/dao/ITestDao.java
@@ -1,13 +1,19 @@
 package com.itcia.itgoo.dao;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
+import com.google.gson.JsonElement;
+import com.itcia.itgoo.dto.Adopt;
 import com.itcia.itgoo.dto.Ex;
 import com.itcia.itgoo.dto.Question;
 import com.itcia.itgoo.dto.Test;
+import com.itcia.itgoo.dto.TestResult;
 
 public interface ITestDao {
 	@Select("select * from question where toggle=1")
@@ -18,6 +24,20 @@ public interface ITestDao {
 	
 	@Insert("insert into test values(${questionnum},'${id}',${dogid},${answer})")
 	void testPaperSubmit(Test t);
+	
+	@Delete("delete from test where id='${id}' and dogid=${dogid}")
+	void testDelete(Test test);
+	@Select("select  question.questionnum,question.correct,question.point," + 
+			"        question.question, test.answer" + 
+			"        from test join question on" + 
+			"        test.questionnum = question.questionnum")
+	List<TestResult> getTestResult();
+	
+	@Update("update adopt set phase=2 where id='${id}' and dogid=${dogid}")
+	void documentPass(Adopt adopt);
+
+	@Delete("delete adopt where id='${id}' and dogid=${dogid}")
+	void adoptOut(Adopt adopt);
 	
 	
 
