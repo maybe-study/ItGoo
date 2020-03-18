5245f6989159cd63ff70da0c6ae44ee6570f1c6a
zzidoc94 <zzidoc94@gmail.com>
2020-03-18 10:38:58 +0900

    경매 조금 함
    경매 폼 만드는중

--- src/main/java/com/itcia/itgoo/ActivityController.java
+++ src/main/java/com/itcia/itgoo/ActivityController.java
@@ -1,28 +1,17 @@
 package com.itcia.itgoo;
 
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-
 import java.security.Principal;
-import java.text.DateFormat;
-import java.util.Date;
-
 import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
